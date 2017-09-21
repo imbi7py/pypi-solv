36c98e6d343534c726cf0c94670bd25748baf5ce
Nick Coghlan <ncoghlan@gmail.com>
2017-09-21 14:57:34 +1000

    Meet flit's publication requirements

---
+++ solv.py
@@ -0,0 +1,15 @@
+"""Placeholder pending proper publication of libsolv bindings"""
+__version__ = "0.0.0"
+
+import warnings
+
+warning_msg = """\
+The libsolv Python bindings are not currently available via PyPI.
+
+Please install them with your distro package manager (typically called
+'python3-solv' or 'python2-solv'), and ensure that any virtual
+environments needing them are configured to be able to see the system
+site packages directory.
+"""
+
+warnings.warn(warning_msg)
